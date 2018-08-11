6748ba755b8b9331fceddeaf600e96ca3dab3e61
Sam Liu <hi1999@gmail.com>
2018-08-11 11:17:35 +0800

    a

---
+++ default.js
@@ -0,0 +1,23 @@
+﻿var linebot = require('linebot');
+var express = require('express');
+
+
+var bot = linebot({
+    channelId: '1585073032',
+    channelSecret: 'e0acb451ddc1d0b89f5e051f78f52739',
+    channelAccessToken: 'llr0iuNKZNnNvDXKQ5SpBGUVSCGM1FxOwEWKFAbiX45ZbKYZVzbLt67TcDwhgjISuTvIdMstBWVpfADfORx7cT0/97Tkx5lmHY2z+77az/3lc371kL27XpD5T9f8Vr+PyLBY4fHmuHrpx8lRyl/HOgdB04t89/1O/w1cDnyilFU='
+});
+
+console.log('start');
+
+
+
+console.log('end');
+
+const app = express();
+const linebotParser = bot.parser();
+app.post('/', linebotParser);
+var server = app.listen(process.env.PORT || 8080, function () {
+    var port = server.address().port;
+    console.log("App now running on port", port);
+});
