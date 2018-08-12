dc136f1436805f45e4f1de5d1a177450321fab83
AnselmBrandon <42286472+AnselmBrandon@users.noreply.github.com>
2018-08-12 15:44:32 +0800

    Update default.js

--- default.js
+++ default.js
@@ -21,6 +21,25 @@ bot.on('message', function (event) {
     console.log('replyToken==>', event.replyToken);
     console.log('userId==>', event.source.userId);
     console.log('==================');
+    if (event.message.text == '0') {
+        event.reply({
+        type: 'template',
+        altText: 'this is a confirm template',
+        template: {
+            type: 'confirm',
+            text: 'Are you sure?',
+            actions: [{
+                type: 'message',
+                label: 'Yes',
+                text: 'yes'
+                }, {
+                    type: 'message',
+                    label: 'No',
+                    text: 'no'
+                }]
+            }//End of template
+        });//End of event.reply
+    }
 });
 
 
