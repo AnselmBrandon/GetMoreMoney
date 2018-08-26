06749d3d0238ccf939ab6a7e1ada6af99f46d4c9
AnselmBrandon <42286472+AnselmBrandon@users.noreply.github.com>
2018-08-26 16:26:51 +0800

    Update default.js

--- default.js
+++ default.js
@@ -131,6 +131,25 @@ bot.on('message', function (event) {
     
     
     if (event.message.text.startsWith('coupon')) {
+        /*
+        
+        var sql1 ="SELECT * FROM public.configure WHERE parameter = 'duration';";
+        console.log(sql1);
+
+        client.query(sql1, (err, res) => {
+        if (err) throw err;
+        for (let row of res.rows) {
+        
+        bot.push(row.lineid, {
+            type: 'text',
+            text: row.msg + row.couponid
+        });
+        console.log('ok');
+         }
+         client.end();
+        });
+        
+        */
         var couponid= event.message.text.replace('coupon', '');
         event.reply({
             "type": "template",
